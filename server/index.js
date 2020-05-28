c892b91d14b1a3266bf73e049db79eaf0272c586
githubSC <sungchang.liang@gmail.com>
2020-05-29 01:18:54 +0800

    heroku deployed

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req,res)=>{
+    res.json({" Deployed": "heroku"});
+});
+
 // get req from auth in auth.js
 //send data from auth to client
 app.get("/api/user/auth", auth, (req, res) => {
